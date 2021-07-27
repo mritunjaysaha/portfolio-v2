81a548a645003f3b418570f1021c6a42b893ce44
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-07-27 11:10:06 +0530

    added theme.js

---
+++ theme.js
@@ -0,0 +1,27 @@
+const color = {
+    black: "#111111",
+    grey: "#767677",
+    lightGrey: "#A4A4A4",
+};
+
+export const theme = {
+    colors: {
+        primary: color.grey,
+        secondary: color.lightGrey,
+    },
+    background: {
+        primary: color.black,
+    },
+    fontSize: {
+        fs12: "1.2rem",
+        fs14: "1.4rem",
+        fs16: "1.6rem",
+        fs18: "1.8rem",
+        fs20: "2rem",
+        fs24: "2.4rem",
+        fs28: "2.8rem",
+        fs32: "3.2rem",
+        fs36: "3.6rem",
+        fs40: "4rem",
+    },
+};
