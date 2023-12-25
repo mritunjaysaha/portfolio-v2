e9e0520da08e2b5c5b889565e49dab575c8dd3f9
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-12-25 18:52:22 +0530

    feat: added head to blogs, projects and project-list pages

--- pages/project-list/index.js
+++ pages/project-list/index.js
@@ -2,7 +2,28 @@ import { projects } from "../../src/projectData/data";
 
 export default function ProjectsList({ selectedProjects }) {
     return (
-        <div>
+        <>
+            <Head>
+                <title>Mritunjay | Project list</title>
+                <meta name="description" content="Mritunjay Saha" />
+                <link rel="icon" href="/favicon.ico" />
+
+                <meta property="og:title" content="Mritunjay Saha" />
+                <meta
+                    property="og:description"
+                    content="Frontend Developer | React.js | TypeScript"
+                />
+                <meta
+                    property="og:image"
+                    content="https://raw.githubusercontent.com/mritunjaysaha/portfolio-v2/main/public/portfolio_og.png"
+                />
+                <meta
+                    property="og:url"
+                    content="https://mritunjaysaha.netlify.app"
+                />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:card" content="Mritunjay" />
+            </Head>
             <ol>
                 {selectedProjects.map(({ name, demo, repo }) => (
                     <li key={name}>
@@ -16,7 +37,7 @@ export default function ProjectsList({ selectedProjects }) {
                     </li>
                 ))}
             </ol>
-        </div>
+        </>
     );
 }
 
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -7,20 +7,43 @@ import { StyledH3 } from "../../src/components/StyledComponents/atoms";
 
 export default function ProjectsPage() {
     return (
-        <ThemeProvider theme={theme}>
-            <main
-                css={css`
-                    height: 100%;
+        <>
+            <Head>
+                <title>Mritunjay</title>
+                <meta name="description" content="Mritunjay Saha" />
+                <link rel="icon" href="/favicon.ico" />
 
-                    ${mediaQueries(bp.desktop)} {
-                        height: 100vh;
-                    }
-                    overflow: hidden;
-                `}
-            >
-                <StyledH3>Projects</StyledH3>
-                <Projects />
-            </main>
-        </ThemeProvider>
+                <meta property="og:title" content="Mritunjay Saha" />
+                <meta
+                    property="og:description"
+                    content="Frontend Developer | React.js | TypeScript"
+                />
+                <meta
+                    property="og:image"
+                    content="https://raw.githubusercontent.com/mritunjaysaha/portfolio-v2/main/public/portfolio_og.png"
+                />
+                <meta
+                    property="og:url"
+                    content="https://mritunjaysaha.netlify.app"
+                />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:card" content="Mritunjay" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <main
+                    css={css`
+                        height: 100%;
+
+                        ${mediaQueries(bp.desktop)} {
+                            height: 100vh;
+                        }
+                        overflow: hidden;
+                    `}
+                >
+                    <StyledH3>Projects</StyledH3>
+                    <Projects />
+                </main>
+            </ThemeProvider>
+        </>
     );
 }
